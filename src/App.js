778db17ae28d79a5d9e75e43e9c769fc75bd7f1a
aldiker <aldiker@ukr.net>
2023-11-17 14:18:03 +0200

    Modifed App and TodoForm to add task from Form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import TodoForm from './components/Todos/TodoForm'
-import TodoList from './components/Todos/TodoList'
 
 function App() {
+    function addTodoToList(todo) {
+        setArrayTodos([...arrayTodos, todo])
+    }
+
+    const [arrayTodos, setArrayTodos] = useState([])
+    console.log(arrayTodos)
+
     return (
         <div className='App'>
             <h1>Todo App</h1>
-            <TodoForm />
-            <TodoList />
+            <TodoForm onClick={addTodoToList} />
         </div>
     )
 }
--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,10 +1,24 @@
-function TodoForm() {
+import { useState } from 'react'
+
+function TodoForm(props) {
+    const { onClick } = props
+
+    const [todoName, setTodoName] = useState('')
+
+    function handleFormSubmit(event) {
+        event.preventDefault()
+        onClick(todoName)
+        setTodoName('')
+    }
+
     return (
         <div className='TodoForm'>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <input
                     type='text'
                     name='todoName'
+                    value={todoName}
+                    onChange={(event) => setTodoName(event.target.value)}
                     placeholder='Enter new todo'
                 />
                 <button type='submit'>Sumbit</button>
