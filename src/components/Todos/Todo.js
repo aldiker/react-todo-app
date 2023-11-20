0a417b2a619456d0d19ea95c96459d7b4e1e0d29
aldiker <aldiker@ukr.net>
2023-11-20 22:26:52 +0200

    Added toggleTodoHandler and use it

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -3,18 +3,25 @@ import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri'
 import { FaCheck } from 'react-icons/fa'
 
 function Todo(props) {
-    const { todo, onDoubleClick } = props
+    const { todo, onDoubleClick, toggleTodo } = props
 
     function delTodo() {
         onDoubleClick(todo.id)
     }
 
     return (
-        <div className={styles.todo} onDoubleClick={delTodo}>
+        <div
+            className={`${styles.todo} ${
+                todo.isCompleted ? styles.completedTodo : ''
+            }`}
+        >
             <RiTodoFill className={styles.todoIcon} />
             <div className={styles.todoText}>{todo.name}</div>
-            <RiDeleteBin2Line className={styles.deleteIcon} />
-            <FaCheck className={styles.checkIcon} />
+            <RiDeleteBin2Line className={styles.deleteIcon} onClick={delTodo} />
+            <FaCheck
+                className={styles.checkIcon}
+                onClick={() => toggleTodo(todo.id)}
+            />
         </div>
     )
 }
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -2,13 +2,18 @@ import Todo from './Todo'
 import styles from './TodoList.module.css'
 
 function TodoList(props) {
-    const { todos, delTodo } = props
+    const { todos, delTodo, toggleTodo } = props
 
     return (
         <div className={styles.todoListContainer}>
             {todos.map((todo) => {
                 return (
-                    <Todo key={todo.id} todo={todo} onDoubleClick={delTodo} />
+                    <Todo
+                        key={todo.id}
+                        todo={todo}
+                        onDoubleClick={delTodo}
+                        toggleTodo={toggleTodo}
+                    />
                 )
             })}
         </div>
