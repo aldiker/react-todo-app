61923e78535f058ed0f74f9aa39af5f48f5087ca
aldiker <aldiker@ukr.net>
2023-11-20 21:29:37 +0200

    Added deleteIcon and checkIcon classes for buttons

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,5 +1,6 @@
 import styles from './Todo.module.css'
-import { RiTodoFill } from 'react-icons/ri'
+import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri'
+import { FaCheck } from 'react-icons/fa'
 
 function Todo(props) {
     const { todo, onDoubleClick } = props
@@ -12,6 +13,8 @@ function Todo(props) {
         <div className={styles.todo} onDoubleClick={delTodo}>
             <RiTodoFill className={styles.todoIcon} />
             <div className={styles.todoText}>{todo.name}</div>
+            <RiDeleteBin2Line className={styles.deleteIcon} />
+            <FaCheck className={styles.checkIcon} />
         </div>
     )
 }
